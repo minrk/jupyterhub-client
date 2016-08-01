db5c49a64d648eea597742e8285ad77291409c79
Min RK <benjaminrk@gmail.com>
2016-08-01 16:55:20 +0200

    make it a package
    trying new approach:
    
    - unconditional setuptools
    - manually disable bdist_egg

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # jupyterhub-client
-JupyterHub REST API client
+
+Example Python client(s) for the JupyterHub REST API.
+There's a synchronous client implemented with Requests,
+and an async client using tornado coroutines.
+
+
+## sync example
+
+```bash
+export JPY_API_TOKEN=`jupyterhub token`
+```
+
+```python
+import os
+from jupyterhub_client import JupyterHubClient
+
+hub = JupyterHubClient(os.environ['JPY_API_TOKEN'])
+users = hub.list_users()
+```
+
+
+## async example
+
+```bash
+export JPY_API_TOKEN=`jupyterhub token`
+```
+
+```python
+import os
+from jupyterhub_client.async import AsyncJupyterHubClient
+from tornado import gen
+
+@gen.coroutine
+def stuff():
+    hub = AsyncJupyterHubClient(os.environ['JPY_API_TOKEN'])
+    users = yield hub.list_users()
+    yield hub.stop_server('bonk')
+```
+
---
+++ setup.py
@@ -0,0 +1,80 @@
+#-----------------------------------------------------------------------------
+#  Copyright (C) Min RK
+#
+#  Distributed under the terms of the 2-clause BSD License.
+#-----------------------------------------------------------------------------
+
+from __future__ import print_function
+
+
+import os
+import sys
+
+from setuptools import setup
+from setuptools.command.bdist_egg import bdist_egg
+from setuptools.command.test import test as TestCommand
+
+name = 'jupyterhub-client'
+pkg_name = name.replace('-', '_')
+
+class EggsNotAllowed(bdist_egg):
+    """
+    Never build eggs.
+    """
+    description = "Building eggs is disabled"
+    def run(self):
+        sys.exit("Refusing to build eggs. Use `pip install`")
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['--cov', pkg_name]
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        pytest.main(self.test_args)
+
+
+with open(os.path.join(pkg_name, '__init__.py')) as f:
+    for line in f:
+        if line.startswith('__version__'):
+            __version__ = eval(line.split('=', 1)[1])
+            break
+
+
+setup_args = dict(
+    name = name,
+    version = __version__,
+    packages = [pkg_name],
+    author = "Min Ragan-Kelley",
+    author_email = "benjaminrk@gmail.com",
+    url = 'http://github.com/minrk/jupyterhub-client',
+    description = "Client for JupyterHub REST API",
+    long_description = "",
+    license = "BSD",
+    install_requires = [
+        'requests',
+        'tornado>=4.1',
+    ],
+    tests_require = [
+        'pytest',
+        'pytest-cov',
+        'jupyterhub',
+        'notebook',
+    ],
+    cmdclass = {
+        'bdist_egg': EggsNotAllowed,
+        'test': PyTest,
+    },
+    classifiers = [
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
+
+setup(**setup_args)
+
